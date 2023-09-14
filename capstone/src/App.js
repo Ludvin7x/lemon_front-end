745ae8e52fbb75c2e7a9124f07b5732089f7e91e
ludvin7x <ludvin@outlook.com>
2023-09-14 14:25:14 -0600

    Week2 ->Semantic Structure

--- capstone/src/App.js
+++ capstone/src/App.js
@@ -1,9 +1,31 @@
 import './App.css';
+import logo from './img/Logo.svg';
+import Header from './Header.js'
+import Nav from './Nav.js'
+import Main from './Main.js'
+import Footer from './Footer'
+import { ReactComponent as Logo } from './img/Logo.svg';
 
 function App() {
   return (
     <div className="App">
-      nombre
+      <Header>
+        <img src={logo} alt="Logo"/>
+        <meta name="description" content="Webpage of Little Lemon Restaurant"/>
+        <meta name="og:title" content="Little Lemon"/>
+        <meta name="og:description" content="Little Lemon Aplication"/>
+        <meta name="og:image" content={logo}/>
+        <Logo />
+        </Header>
+      <Nav>
+        <ul>
+          <li>hola</li>
+          <li>hola</li>
+          <li>hola</li>
+        </ul>
+      </Nav>
+      <Main></Main>
+      <Footer></Footer>
     </div>
   );
 }
---
+++ capstone/src/Header.js
@@ -0,0 +1,11 @@
+import './App.css';
+
+function Header() {
+  return (
+    <div className="Header">
+
+    </div>
+  );
+}
+
+export default Header;
